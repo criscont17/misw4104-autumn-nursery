079236ccda0e02aada0390ebebd918c43e78f9b9
Cristhian Contreras <c.contrerasb@uniandes.edu.co>
2025-04-25 20:45:30 -0500

    feat(component): creacion de componente esqueleto para listar plantas

--- src/app/plants/plants.module.ts
+++ src/app/plants/plants.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PlantListComponent } from './components/plant-list/plant-list.component';
+
 @NgModule({
-  declarations: [],
+  declarations: [PlantListComponent],
   imports: [CommonModule],
 })
 export class PlantsModule {}
