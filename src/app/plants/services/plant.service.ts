aff15109d957fedfe822446fded9a98bb86b8f43
Cristhian Contreras <c.contrerasb@uniandes.edu.co>
2025-04-26 12:14:16 -0500

    feat(service): consulta de listado de plantas en API

--- src/app/plants/services/plant.service.ts
+++ src/app/plants/services/plant.service.ts
@@ -1,8 +1,45 @@
+import { catchError, map, Observable, throwError } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { environment } from '../../../environments/environment.development';
+import { Plant } from '../models/plant';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlantService {
-  constructor() {}
+  private _endpointUrl = `${environment.apiUrl}/cf1077fa69112bc67ff520dd6517a93afd3dae29/202212_MISW4104_Grupo2.json`;
+
+  constructor(private _http: HttpClient) {}
+
+  getPlants(): Observable<any> {
+    return this._http.get<Plant[]>(this._endpointUrl).pipe(
+      map((plants) => {
+        let indoorPlant = 0;
+        let outdoorPlant = 0;
+
+        plants.forEach((plant) => {
+          if (plant.tipo === 'Interior') indoorPlant += 1;
+          if (plant.tipo === 'Exterior') outdoorPlant += 1;
+        });
+
+        return {
+          indoorPlant,
+          outdoorPlant,
+          plants,
+        };
+      }),
+      catchError((error) => {
+        console.error('Error al consultar API:', error);
+
+        return throwError(
+          () =>
+            new Error(
+              'No se pudo obtener el listado de plantas. Por favor intente más tarde.'
+            )
+        );
+      })
+    );
+  }
 }
