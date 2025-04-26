83d8ddb909c0d5fb91aa3fc9981f32441062b51f
Cristhian Contreras <c.contrerasb@uniandes.edu.co>
2025-04-26 13:39:13 -0500

    feat(controlador): implemento logica para consultar y detallar informacion

--- src/app/plants/components/plant-list/plant-list.component.ts
+++ src/app/plants/components/plant-list/plant-list.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { PlantService } from '../../services/plant.service';
+import { Plant } from '../../models/plant';
 
 @Component({
   selector: 'app-plant-list',
   standalone: false,
   templateUrl: './plant-list.component.html',
-  styleUrl: './plant-list.component.css'
+  styleUrl: './plant-list.component.css',
 })
-export class PlantListComponent {
+export class PlantListComponent implements OnInit {
+  errorMessage = '';
+  indoorPlants = 0;
+  outdoorPlants = 0;
+  plants: Plant[] = [];
 
+  constructor(private plantService: PlantService) {}
+
+  ngOnInit(): void {
+    this.loadNurseryDetail();
+  }
+
+  loadNurseryDetail(): void {
+    this.plantService.fetchNurseryDetail$().subscribe({
+      next: ({ indoorPlant, outdoorPlant, plants }) => {
+        this.indoorPlants = indoorPlant;
+        (this.outdoorPlants = outdoorPlant), (this.plants = plants);
+      },
+      error: (error) => {
+        this.errorMessage = error.message;
+      },
+    });
+  }
 }
